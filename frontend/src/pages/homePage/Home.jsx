690fe1e6d4d8983c8fc3c556e0e429d728cf3f87
sbrycbc <sabriyecbc@gmail.com>
2023-10-17 11:35:31 +0200

    updated home page...

--- frontend/src/pages/homePage/Home.jsx
+++ frontend/src/pages/homePage/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
       <Row>
         <Col lg="6" md="6">
           <h4 className='p-2'>Home Page</h4>
+          <h4 className='p-2'>Home Page</h4>
+
         </Col>
       </Row>
     </Container>
