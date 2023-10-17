44c7b432bfe6aa51343ed9c3e4f5bd6c262874e5
sbrycbc <sabriyecbc@gmail.com>
2023-10-17 11:26:06 +0200

    added new homepage..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './component/Footer/Footer.jsx'
+import Home from './pages/homePage/Home.jsx';
 
 function App() {
   return (
     <div>
       <Footer />
+      <Home />
     </div>
   );
 }
