82a9ecf512070f265188a6958bb25e98d857d9b1
sbrycbc <sabriyecbc@gmail.com>
2023-10-18 15:37:59 +0200

    added home-page style with images for carousel..

--- frontend/src/pages/homePage/Home.jsx
+++ frontend/src/pages/homePage/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import './Home.scss'
 import { Container, Row, Col } from "reactstrap";
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import Garten from '../../assets/Garten.jpg';
-import Book from '../../assets/Book-01.jpg';
-import Book03 from '../../assets/Book-3.jpg';
-
-import Spielzeug from '../../assets/Spielzeug-2.jpg'
+import Kleidung from '../../assets/Kleidung.jpg';
+import Book03 from '../../assets/Book-03.jpg';
+import Spielzeug from '../../assets/Spielzeug-03.jpg';
 
 
 const Home = () => {
@@ -16,33 +16,35 @@ const Home = () => {
     <Container>
       <Row>
         <Col lg="6" md="6">
-          <div className='text_content mt-4'>
-            <h4 className='mb-2 '>
+          <div className='text_content'>
+            <h2 className='logo_text mb-4 '>
             Sharing is Caring - 
-            </h4>
+            </h2>
             <h2 className="mb-4 text_title">
-                  <span>Teaching our children to share is teaching  them </span> <br /> 
-                  <span> compassion and love.</span>
+                  <span>" Teaching our children to share is teaching  them </span> <br /> 
+                  <span> compassion and love. "</span>
             </h2>
             <p>Kevin Heath</p>
           </div>
-          <div className="text_btns d-flex align-items-center gap-5 mt-3">
-          <button className="bestellt_btn d-flex align-items-center justify-content-between">
+          <div className="">
+          <button className="category_btn d-flex justify-content-center ">
             Category
-                <HiArrowNarrowRight className='ml- my-1 p-1' />
+                <span className='category_icon'>
+                  <HiArrowNarrowRight/>
+                </span>
           </button>  
           </div>
           
         </Col>
         <Col lg="6" md="6" >
           <Carousel className='m-2 p-1'>
-              <div className=''>
-                  <img src={Garten} alt="Legend-1"/>
+              <div>
+                  <img src={Garten} alt="Legend-1" />
                   <p className="legend">Garten</p>
               </div>
               <div>
-                  <img src={Book} alt="Legend-1" />
-                  <p className="legend">Book</p>
+                  <img src={Kleidung} alt="Legend-1"/>
+                  <p className="legend">Kleidung</p>
               </div>
               <div>
                   <img src={Book03} alt="Legend-1" />
