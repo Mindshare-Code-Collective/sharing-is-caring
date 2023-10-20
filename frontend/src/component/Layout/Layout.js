ebdec35abcb03ce73d94354b74aa01d0f2174e2b
benni <bstolp@gmx.de>
2023-10-20 09:59:55 +0200

    remove lupe in dashboard

--- frontend/src/component/Layout/Layout.js
+++ frontend/src/component/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Routers from '../../routes/Routers';
+
 const Layout = () => {
   return (
     <div>
--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from "reactstrap";
 import './dashboard.scss';
-import Lupe from './assets/suche.png';
+
 
 const Dashboard = () => {
   return (
