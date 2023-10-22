73e4f2aa0c1ea475af17605e3e2118ae09587a60
sbrycbc <sabriyecbc@gmail.com>
2023-10-22 20:03:41 +0200

    added userModel and installed bcrypt...

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+);
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    //console.log("pasword 1", user.password)
+    bcrypt.hash(user.password, 10, (err, hash) => {
+      user.password = hash;
+    //console.log("pasword 2", user.password)
+  
+      next();
+    });
+ });
+
+
+
+
+const User = mongoose.model('User', userSchema);
+
+
+export default User;
