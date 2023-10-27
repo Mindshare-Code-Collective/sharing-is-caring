db23f25e9fd2a542d904b07440a2606b616703bb
benni <bstolp@gmx.de>
2023-10-27 09:40:52 +0200

    fixed prob between userOffersForm und usersOffersList, zusammenführung mittels useState im Dashboard

--- frontend/src/pages/dashboard/dashboard_components/UserOffersForm.jsx
+++ frontend/src/pages/dashboard/dashboard_components/UserOffersForm.jsx
@@ -1,43 +1,80 @@
 import React, { useState } from 'react'
 
-export default function UserOffersForm(/* { userProducts, setUserProducts } */) {
-  const [userProducts, setUserProducts ] = useState([]);
+export default function UserOffersForm({ onProductSubmit }) {
+  const [userProduct, setUserProducts ] = useState({
+    name: '',
+    kategorie: '',
+    zustand: '',
+    zustellung: '',
+    sonstiges: '',
+    image: '',
+    userContact: ''
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setUserProducts([...userProducts,
-    {
-        name: e.target.name.value,
-        kategorie: e.target.kategorie.value,
-        zustand: e.target.zustand.value,
-        zustellung: e.target.zustellung.value,
-        sonstiges: e.target.sonstiges.value,
-        image: e.target.image.value,
-        userContact: e.target.usercontact.value, 
-      },      
-    ]);
+    onProductSubmit(userProduct);
+    e.target.reset();
+  }
 
-    e.target.reset(); // Zurücksetzen der Eingabefelder
-  };
   return (
     <div>
     <div className="user-product-form">
       <h4>Add a product:</h4>
       <form onSubmit={handleSubmit}>
         <p>Name des Artikels:</p>
-        <input type={"text"} name="name" />
+        <input 
+          type="text" 
+          name="name"
+          required 
+          value={userProduct.name}
+          onChange={(e) => setUserProducts({ ...userProduct, name: e.target.value })} 
+        />
         <p>Kategorie:</p>
-        <input type={"text"} name="kategorie" />
+        <input 
+          type="text" 
+          name="kategorie"
+          required 
+          value={userProduct.kategorie}
+          onChange={(e) => setUserProducts({ ...userProduct, kategorie: e.target.value })} 
+        />
         <p>Zustand:</p>
-        <input type={"text"} name="zustand" />
+        <input 
+          type={"text"} 
+          name="zustand" 
+          value={userProduct.zustand}
+          onChange={(e) => setUserProducts({ ...userProduct, zustand: e.target.value})}  
+        />
         <p>Zustellung:</p>
-        <input type={"text"} name="zustellung" />
+        <input 
+          type={"text"} 
+          name="zustellung"
+          required
+          value={userProduct.zustellung}
+          onChange={(e) => setUserProducts({ ...userProduct, zustellung: e.target.value})} 
+        />
         <p>Sonstige Infos:</p>
-        <input type={"text"} name="sonstiges" />
+        <input 
+          type={"text"} 
+          name="sonstiges"
+          value={userProduct.sonstiges}
+          onChange={(e) => setUserProducts({ ...userProduct, sonstiges: e.target.value})} 
+        />
         <p>Bild:</p>
-        <input type={"text"} name="image" />
-        <p>usercontact:</p>
-        <input type={"email"} name="usercontact" />
+        <input 
+          type={"text"} 
+          name="image" 
+          value={userProduct.image}
+          onChange={(e) => setUserProducts({ ...userProduct, image: e.target.value})}
+        />
+        <p>User Kontakt:</p>
+        <input 
+          type={"email"} 
+          name="usercontact"
+          required 
+          value={userProduct.usercontact}
+          onChange = {(e) => setUserProducts({ ...userProduct, usercontact: e.target.value })} 
+        />
         <br></br>
         <button>add</button>
       </form>
--- frontend/src/pages/dashboard/dashboard_components/UserOffersList.jsx
+++ frontend/src/pages/dashboard/dashboard_components/UserOffersList.jsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import UserOffersForm from './UserOffersForm';
 
 
-export default function UserOffersList() {
-  const [ userProducts, setUserProducts ] = useState([]);
+export default function UserOffersList({userProducts}) {
 
   return (
     <div>
       <h2>UserOffersList</h2>
-      {
-        userProducts.map((product, index) => (
-          <div key={index}>
-            <p>Produkt Name: {product.name}</p>
-            
-
+        {userProducts.map((product, index) => (
+          <div key={index} style={{border: "1px solid black"}} >
+            <p>Name des Artikels: {product.name}</p>
+            <p>Kategorie: {product.kategorie}</p>
+            <p>Zustand: {product.zustand}</p>
+            <p>Zustellung: {product.zustellung}</p>
+            <p>Sonstige Infos: {product.sonstiges}</p>
+            <p>Bild: {product.image}</p>
+            <p>User Kontakt: {product.usercontact}</p>
           </div>
         ))}
     </div>
