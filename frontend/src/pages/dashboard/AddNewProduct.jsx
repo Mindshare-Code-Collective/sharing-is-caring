0039ff2bdb0e24c8e2b7253e988aa2d7aac077e7
miaadabbas <miaad.abbas@yahoo.com>
2023-11-13 22:52:30 +0100

    add more category-change position of searchBar

--- frontend/src/pages/dashboard/AddNewProduct.jsx
+++ frontend/src/pages/dashboard/AddNewProduct.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useContext } from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import axios from 'axios';
-import { AppContext } from '../../AppContext';
-import config from '../../component/config/config';
+import React, { useState, useContext } from "react";
+import { Button, Modal } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { AppContext } from "../../AppContext";
+import config from "../../component/config/config";
 
 export default function AddNewProduct(props) {
   const { showModal, setShowModal, userInfo } = props;
   const { addProductToState } = useContext(AppContext);
 
   const [userProduct, setUserProducts] = useState({
-    name: '',
-    category: '',
-    trade: '',
-    condition: '',
-    shipment: '',
-    description: '', // Add description to the state
-    picture: '',
+    name: "",
+    category: "",
+    trade: "",
+    condition: "",
+    shipment: "",
+    description: "", // Add description to the state
+    picture: "",
     user: userInfo?.id,
   });
 
@@ -27,7 +27,7 @@ export default function AddNewProduct(props) {
   const handleChange = (e) => {
     const { name, value, type, files } = e.target;
 
-    if (type === 'file') {
+    if (type === "file") {
       const file = files[0];
       const reader = new FileReader();
       if (file) {
@@ -38,7 +38,7 @@ export default function AddNewProduct(props) {
           setUserProducts((prevUserProduct) => ({
             ...prevUserProduct,
             picture: base64Data,
-          }));  
+          }));
           //console.log('Base64 data:', base64Data);
         };
         reader.onerror = (error) => {
@@ -55,7 +55,7 @@ export default function AddNewProduct(props) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const requestData = {
       name: userProduct.name,
       category: userProduct.category,
@@ -66,30 +66,34 @@ export default function AddNewProduct(props) {
       user: userProduct.user,
       picture: userProduct.picture,
     };
-  
+
     try {
-      const response = await axios.post(config.routes.product.createProduct, requestData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
+      const response = await axios.post(
+        config.routes.product.createProduct,
+        requestData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       if (response.data.success) {
-        console.log('Data successfully sent!', response.data);
+        console.log("Data successfully sent!", response.data);
         setShowModal(!showModal);
-      
+
         // Add the new product to the products state in context
         addProductToState(response.data);
-        
+
         // Reload the page
         window.location.reload();
       } else {
-        console.error('Error sending data:', response.data.error.message);
+        console.error("Error sending data:", response.data.error.message);
       }
     } catch (error) {
-      console.error('Error sending data:', error);
+      console.error("Error sending data:", error);
     }
-    console.log('Reached the end of handleSubmit');
+    console.log("Reached the end of handleSubmit");
   };
 
   return (
@@ -98,20 +102,41 @@ export default function AddNewProduct(props) {
         <Modal.Title>Add Neues Produkt</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form id="request" className="main_form" method="POST" action="http://localhost:3333/products/">
-        <Form.Group className="mb-2">
-            <Form.Label>Name des Artikels
-              <span className="text-danger">  *</span>
+        <Form
+          id="request"
+          className="main_form"
+          method="POST"
+          action="http://localhost:3333/products/"
+        >
+          <Form.Group className="mb-2">
+            <Form.Label>
+              Name des Artikels
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="name" type="text" name="name" placeholder="Name" required
-              onChange={handleChange} />
+            <Form.Control
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+              onChange={handleChange}
+            />
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Kategorie
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Kategorie
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="category" name="category" as="select" required onChange={handleChange}>
-              <option value="" disabled selected>Bitte auswählen:</option>
+            <Form.Control
+              id="category"
+              name="category"
+              as="select"
+              required
+              onChange={handleChange}
+            >
+              <option value="" disabled selected>
+                Bitte auswählen:
+              </option>
               <option value="garten">Garten</option>
               <option value="bücher">Bücher</option>
               <option value="dekoration">Dekoration</option>
@@ -122,47 +147,87 @@ export default function AddNewProduct(props) {
             </Form.Control>
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Trade
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Trade
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="trade" name="trade" as="select" required onChange={handleChange}>
-              <option value="" disabled selected>Bitte auswählen:</option>
+            <Form.Control
+              id="trade"
+              name="trade"
+              as="select"
+              required
+              onChange={handleChange}
+            >
+              <option value="" disabled selected>
+                Bitte auswählen:
+              </option>
               <option value="Tauschen">Tauschen</option>
               <option value="Verschenken">Verschenken</option>
             </Form.Control>
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Zustand
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Zustand
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="condition" name="condition" as="select" required onChange={handleChange}>
-              <option value="" disabled selected>Bitte auswählen:</option>
+            <Form.Control
+              id="condition"
+              name="condition"
+              as="select"
+              required
+              onChange={handleChange}
+            >
+              <option value="" disabled selected>
+                Bitte auswählen:
+              </option>
               <option value="Neu">Neu</option>
               <option value="Gebraucht">Gebraucht</option>
             </Form.Control>
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Zustellung
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Zustellung
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="shipment" name="shipment" as="select" required onChange={handleChange}>
-              <option value="" disabled selected>Bitte auswählen:</option>
+            <Form.Control
+              id="shipment"
+              name="shipment"
+              as="select"
+              required
+              onChange={handleChange}
+            >
+              <option value="" disabled selected>
+                Bitte auswählen:
+              </option>
               <option value="Abholung">Abholung</option>
               <option value="Versand">Versand</option>
             </Form.Control>
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Beschreibung
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Beschreibung
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="description" as="textarea" name="description" placeholder="Beschreibung" required
-              onChange={handleChange} />
+            <Form.Control
+              id="description"
+              as="textarea"
+              name="description"
+              placeholder="Beschreibung"
+              required
+              onChange={handleChange}
+            />
           </Form.Group>
           <Form.Group className="mb-2">
-            <Form.Label>Wählen Sie ein Bild aus
-              <span className="text-danger">  *</span>
+            <Form.Label>
+              Wählen Sie ein Bild aus
+              <span className="text-danger"> *</span>
             </Form.Label>
-            <Form.Control id="picture" type="file" name="picture" onChange={handleChange} />
+            <Form.Control
+              id="picture"
+              type="file"
+              name="picture"
+              onChange={handleChange}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -173,4 +238,4 @@ export default function AddNewProduct(props) {
       </Modal.Footer>
     </Modal>
   );
-};
+}
--- frontend/src/pages/homePage/Home.jsx
+++ frontend/src/pages/homePage/Home.jsx
@@ -1,56 +1,61 @@
-import React, { useState, useEffect, useContext } from 'react';
-import './Home.scss';
-import { Container, Row, Col, Input } from 'reactstrap';
+import React, { useState, useEffect, useContext } from "react";
+import "./Home.scss";
+import { Container, Row, Col, Input } from "reactstrap";
 // import { HiArrowNarrowRight } from 'react-icons/hi';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-import Garten from '../../assets/Garten.jpg';
-import Kleidung from '../../assets/Kleidung.jpg';
-import Alle from '../../assets/Alle.png';
-import Elektronik from '../../assets/Elektronik.png';
-import Book03 from '../../assets/Book-03.jpg';
-import Spielzeug from '../../assets/Spielzeug-03.jpg';
-import Spielzeug2 from '../../assets/Spielzeug-2.jpg';
-import Decoration from '../../assets/Decoration.jpg';
-import Book from '../../assets/Book.jpg';
-import ProductCard from '../dashboard/ProductCard';
-import { AppContext } from '../../AppContext';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Garten from "../../assets/Garten.jpg";
+import Kleidung from "../../assets/Kleidung.jpg";
+import Alle from "../../assets/Alle.png";
+import Elektronik from "../../assets/Elektronik.png";
+import Book03 from "../../assets/Book-03.jpg";
+import Spielzeug from "../../assets/Spielzeug-03.jpg";
+import Spielzeug2 from "../../assets/Spielzeug-2.jpg";
+import Decoration from "../../assets/Decoration.jpg";
+import Book from "../../assets/Book.jpg";
+import ProductCard from "../dashboard/ProductCard";
+import { AppContext } from "../../AppContext";
 
 const Home = () => {
   const { products } = useContext(AppContext);
-  const [selectedCategory, setSelectedCategory] = useState('Alle');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("Alle");
+  const [searchTerm, setSearchTerm] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   const categoryNames = {
-    All: 'Alle',
-    Garten: 'Garten',
-    Book: 'Bücher',
-    Decoration: 'Dekoration',
-    Clothes: 'Kleidung',
-    Schuhe: 'Schuhe',
-    Spielzeug:'Spielzeug',
-    Elektronik: 'Elektronik',
+    All: "Alle",
+    Garten: "Garten",
+    Book: "Bücher",
+    Decoration: "Dekoration",
+    Clothes: "Kleidung",
+    Schuhe: "Schuhe",
+    Spielzeug: "Spielzeug",
+    Elektronik: "Elektronik",
   };
 
   useEffect(() => {
     if (Array.isArray(products)) {
       let filtered = [...products];
-  
+
       // Filter by category
       if (selectedCategory !== categoryNames.All) {
-        filtered = filtered.filter((product) => product.category.toLowerCase() === selectedCategory.toLowerCase());
+        filtered = filtered.filter(
+          (product) =>
+            product.category.toLowerCase() === selectedCategory.toLowerCase()
+        );
       }
-  
+
       // Filter by search term
-      if (searchTerm.trim() !== '') {
+      if (searchTerm.trim() !== "") {
         const lowerCaseSearchTerm = searchTerm.toLowerCase();
-        filtered = filtered.filter((product) => product.name.toLowerCase().includes(lowerCaseSearchTerm));
+        filtered = filtered.filter((product) =>
+          product.name.toLowerCase().includes(lowerCaseSearchTerm)
+        );
       }
-  
+
       setFilteredProducts(filtered);
     }
-  }, [selectedCategory, searchTerm, products, categoryNames.All]);    
+  }, [selectedCategory, searchTerm, products, categoryNames.All]);
 
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
@@ -64,7 +69,11 @@ const Home = () => {
     return Object.values(categoryNames).map((category, index) => (
       <button
         key={index}
-        className={`category_button ${selectedCategory.toLowerCase() === category.toLowerCase() ? 'active' : ''}`}
+        className={`category_button ${
+          selectedCategory.toLowerCase() === category.toLowerCase()
+            ? "active"
+            : ""
+        }`}
         onClick={() => handleCategoryChange(category)}
       >
         <img src={getImageByCategory(category)} alt={`${category} Category`} />
@@ -76,21 +85,21 @@ const Home = () => {
   const getImageByCategory = (category) => {
     switch (category) {
       case categoryNames.Garten:
-        return Garten || 'Garten';
+        return Garten || "Garten";
       case categoryNames.Book:
-        return Book || 'Book';
+        return Book || "Book";
       case categoryNames.Decoration:
-        return Decoration || 'Decoration';
+        return Decoration || "Decoration";
       case categoryNames.Clothes:
-        return Kleidung || 'Clothes';
-        case categoryNames.Schuhe:
-          return Spielzeug2 || 'Schuhe';
-        case categoryNames.Spielzeug:
-          return Spielzeug || 'Spielzeug';
-          case categoryNames.Elektronik:
-            return Elektronik || 'Elektronik';
+        return Kleidung || "Clothes";
+      case categoryNames.Schuhe:
+        return Spielzeug2 || "Schuhe";
+      case categoryNames.Spielzeug:
+        return Spielzeug || "Spielzeug";
+      case categoryNames.Elektronik:
+        return Elektronik || "Elektronik";
       case categoryNames.All:
-        return Alle || 'Alle';
+        return Alle || "Alle";
       default:
         return category;
     }
@@ -102,18 +111,22 @@ const Home = () => {
         <Container>
           <Row>
             <Col lg="6" md="6">
-              <div className='text_content'>
-                <h2 className='logo_text mb-4 '>
-                  Sharing is Caring -
-                </h2>
+              <div className="text_content">
+                <h2 className="logo_text mb-4 ">Sharing is Caring -</h2>
                 <h2 className="mb-4 text_title">
-                  <span>Teaching our children to share is teaching them </span> <br />
+                  <span>Teaching our children to share is teaching them </span>{" "}
+                  <br />
                   <span> compassion and love. </span>
                 </h2>
                 <p>Kevin Heath</p>
               </div>
               <div className="search-bar">
-                  <Input type="text" placeholder="Search by product name" value={searchTerm} onChange={handleSearchChange} />
+                <Input
+                  type="text"
+                  placeholder="Search by product name"
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
               </div>
             </Col>
             <Col lg="6" md="6">
@@ -147,12 +160,14 @@ const Home = () => {
               <h2 className="text-center">POPULAR CATEGORIES</h2>
               <div className="popular_category d-flex align-items-center justify-content-center gap-3">
                 {renderCategoryButtons()}
-
               </div>
             </Col>
           </Row>
           <Row>
-            {filteredProducts && filteredProducts.map((product, index) => <ProductCard key={index} product={product} />)}
+            {filteredProducts &&
+              filteredProducts.map((product, index) => (
+                <ProductCard key={index} product={product} />
+              ))}
           </Row>
         </Container>
       </section>
