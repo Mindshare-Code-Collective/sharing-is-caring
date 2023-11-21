d888d363d41e877dd6e23bd4e74eded26d4a5b03
miaadabbas <miaad.abbas@yahoo.com>
2023-11-21 22:04:26 +0100

    add-hover-product-card

--- frontend/src/component/Footer/Footer.jsx
+++ frontend/src/component/Footer/Footer.jsx
@@ -3,23 +3,26 @@ import { Container, Col, Row } from "react-bootstrap";
 import { FaInstagram } from "react-icons/fa6";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { RiTwitterXLine } from "react-icons/ri";
-import { LiaFacebook } from "react-icons/lia";
+
 
 import "./Footer.scss";
 const Footer = () => {
   return (
-    <div className="d-flex justify-content-between footer mt-4 p-2">
+    <div className="footer mt-5 p-2 ">
       <Container>
-        <Row>
+        <Row  >
+          <Col>
+            <p className="mt-2">© 2023 SCHARING IS CARING™</p>
+          </Col>
           <Col className="icons d-flex ">
             <div>
-              <a style={{fontSize:'30px'}} href="https://www.instagram.com/">
+              <a  href="https://www.instagram.com/">
                 <FaInstagram />
               </a>
             </div>
             <div>
-              <a style={{fontSize:'30px'}} href="https://www.facebook.com/">
-                <FaSquareFacebook/>
+              <a  href="https://www.facebook.com/">
+                <FaSquareFacebook />
               </a>
             </div>
             <div>
@@ -27,12 +30,6 @@ const Footer = () => {
                 <RiTwitterXLine />
               </a>
             </div>
-          </Col>
-
-          <Col>
-          
-            <p className="mt-2">© 2023 SCHARING IS CARING™</p>
-            
           </Col>
         </Row>
       </Container>
